b95f309b9dbef9464f7dd17897d27ffc595d2889
hasscode <sona72003@gmail.com>
2025-08-18 12:01:05 +0300

    finishing movies domain data layer : movies_remote_data_source movies_repository Movie_model

---
+++ lib/movies/data/repositories/movies_repository.dart
@@ -0,0 +1,33 @@
+import 'package:dartz/dartz.dart';
+import 'package:movie_app/movies/data/data_source/movies_remote_data_source.dart';
+import 'package:movie_app/movies/domain/entities/movie.dart';
+import 'package:movie_app/movies/domain/repositories/base_movies_repository.dart';
+
+import '../../../core/error handling/failures.dart';
+
+class MoviesRepository implements BaseMoviesRepository{
+  MovieBaseRemoteDataSource movieBaseRemoteDataSource;
+
+  MoviesRepository(this.movieBaseRemoteDataSource);
+
+  @override
+  Future<Either<Failures, List<Movie>>> getNowPlaying() async{
+    final  result = await movieBaseRemoteDataSource.getNowPlayingMovies();
+    return result;
+
+  }
+
+  @override
+  Future<Either<Failures, List<Movie>>>  getPopular() async{
+    final  result = await movieBaseRemoteDataSource.getPopularMovies();
+    return result;
+  }
+
+  @override
+  Future<Either<Failures, List<Movie>>>  getTopRated() async{
+    final  result = await movieBaseRemoteDataSource.getTopRatedMovies();
+    return result;
+  }
+
+
+}
